a34436e4f42c9dcfd4e4e1d9795f3c9e6f48ffd0
Dranik1 <125028857+Dranik1@users.noreply.github.com>
2024-11-25 09:19:17 +0200

    h

--- 251124.py
+++ 251124.py
@@ -14,17 +14,24 @@ print('Datu bāze ir izveidota')
 
 print("Tabula ir izveidota!")'''
 
-conn.execute('''INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY)  
-             VALUES(1, 'Paul', 32, 'California', 20000.0)''')
+'''conn.execute(INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY)  
+             VALUES(1, 'Paul', 32, 'California', 20000.0))
 
-conn.execute('''INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY)  
-             VALUES(2, 'Alex', 25, 'Latvia', 15000.0)''')
+conn.execute(INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY)  
+             VALUES(2, 'Alex', 25, 'Latvia', 15000.0))
 
-conn.execute('''INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY)  
-             VALUES(3, 'Teddy', 23, 'Norway', 20000.0)''')
+conn.execute(INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY)  
+             VALUES(3, 'Teddy', 23, 'Norway', 20000.0))
 
-conn.execute('''INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY)  
-             VALUES(4, 'Mark', 25, 'Germany', 65000.0)''')
+conn.execute(INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY)  
+             VALUES(4, 'Mark', 25, 'Germany', 65000.0))
+
+conn.commit()'''
+
+cursor = conn.execute("SELECT * from COMPANY")
+for i in cursor:
+    print(i)
+
 
 
 
