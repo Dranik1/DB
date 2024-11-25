67d2f7ae1946f24dd8613c5cf46a3ded44af8dfc
Dranik1 <125028857+Dranik1@users.noreply.github.com>
2024-11-25 09:09:13 +0200

    Update 251124.py

--- 251124.py
+++ 251124.py
@@ -3,12 +3,29 @@ import sqlite3
 conn = sqlite3.connect('test.db')
 print('Datu bāze ir izveidota')
 
-conn.execute('''CREATE TABLE COMPANY
+'''conn.execute(CREATE TABLE COMPANY
              (ID INT PRIMARY KEY    NOT NULL,
              NAME       TEXT    NOT NULL,
              AGE        INT NOT NULL,
              ADDRESS    CHAR(50),
-             SALARY     REAL);''')
+             SALARY     REAL);)
 
-print("Tabula ir izveidota!")
+
+
+print("Tabula ir izveidota!")'''
+
+conn.execute('''INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY)  
+             VALUES(1, 'Paul', 32, 'California', 20000.0)''')
+
+conn.execute('''INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY)  
+             VALUES(2, 'Alex', 25, 'Latvia', 15000.0)''')
+
+conn.execute('''INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY)  
+             VALUES(3, 'Teddy', 23, 'Norway', 20000.0)''')
+
+conn.execute('''INSERT INTO COMPANY (ID, NAME, AGE, ADDRESS, SALARY)  
+             VALUES(4, 'Mark', 25, 'Germany', 65000.0)''')
+
+
+
 conn.close()
